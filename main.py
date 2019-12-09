623247b3d8ca9126258a794fd3b5790b9b7465d5
eurekaqq <npes89033@gmail.com>
2019-12-09 22:56:18 +0900

    change ui library from remi to tkinter

--- main.py
+++ main.py
@@ -1,101 +1,117 @@
-import sys
 import os
+import sys
+import tkinter as tk
 import unicodedata
-import remi.gui as gui
-from remi import start, App
+import webbrowser
+from tkinter import filedialog, messagebox
 
-class M3uGenerator(App):
-    def __init__(self, *args):
-        super(M3uGenerator, self).__init__(*args)
 
-    def on_select_btn_pressed(self, widget):
-        self.folder_selection_dialog = gui.FileSelectionDialog('Folder Selection Dialog',
-                                                'Select folder to generate m3u', False,
-                                                '.', False)
-        self.folder_selection_dialog.confirm_value.do(
-            self.on_fileselection_dialog_confirm)
+class M3uGenerator(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master=master)
+        self.master = master
+        self.pack()
+        self.init_widget()
+        self.selected_folder = None
 
-        # here is returned the Input Dialog widget, and it will be shown
-        self.folder_selection_dialog.show(self)
+    def init_widget(self):
+        self.path_label = tk.Label(self,
+                                   text=r'Please select music folder',
+                                   font=('', 20),
+                                   wraplength=500)
+        self.path_label.grid(row=1,
+                             columnspan=5,
+                             sticky=tk.N,
+                             ipadx=15,
+                             padx=15,
+                             pady=15)
 
-    def on_fileselection_dialog_confirm(self, widget, filelist):
-        if len(filelist) == 0:
-            self.lbl.set_text('Please choose a folder')
+        self.select_button = tk.Button(self,
+                                       text='Select folder',
+                                       font=('', 22),
+                                       command=self.selection_folder)
+        self.select_button.grid(row=2, column=1, pady=5)
+
+        self.gen_button = tk.Button(self,
+                                    text='Go',
+                                    font=('', 22),
+                                    command=self._gen_m3u)
+        self.gen_button.grid(row=2, column=3, pady=5)
+
+        self.fork_me_label = tk.Label(self,
+                                      text=r'Fork me here',
+                                      font=('', 18),
+                                      fg='blue',
+                                      cursor='hand2')
+        self.fork_me_label.grid(row=3, columnspan=5, sticky=tk.S, pady=15)
+        self.fork_me_label.bind('<Button-1>', self._fork_me)
+
+    def _fork_me(self, *_):
+        webbrowser.open_new_tab(r'https://github.com/npes87184/PyM3UGenerator')
+
+    def selection_folder(self):
+        self.selected_folder = filedialog.askdirectory(initialdir=r'./')
+
+        if os.path.exists(self.selected_folder):
+            self.path_label['text'] = self.selected_folder
         else:
-            self.lbl.set_text(filelist[0])
+            self.path_label['text'] = r'Please select music folder first'
 
-    def on_go_btn_pressed(self, widget):
-        doing_dialog = gui.GenericDialog("", "Doing", width=500, height=100)
-        doing_dialog.show(self)
-        m3u_path = self.gen_m3u()
-        doing_dialog.hide()
-        done_dialog = gui.GenericDialog("", "m3u created at [{}]".format(m3u_path), width=500, height=100)
-        done_dialog.show(self)
-
-    def gen_m3u(self):
-        select_dir = self.lbl.get_text()
-        if not os.path.isdir(select_dir):
+    def _gen_m3u(self):
+        if not self.selected_folder or not os.path.isdir(self.selected_folder):
+            messagebox.showwarning(r'Warning',
+                                   r'Please select music folder first')
             return
 
         def is_music(path):
-            support_extension = ['.mp3', '.m3u', '.wma', '.flac',
-				'.wav', '.mc', '.aac', '.m4a',
-				'.ape', '.dsf', '.dff']
+            support_extension = {
+                '.mp3', '.m3u', '.wma', '.flac', '.wav', '.mc', '.aac', '.m4a',
+                '.ape', '.dsf', '.dff'
+            }
             filename, extension = os.path.splitext(path)
             if extension.lower() in support_extension:
                 return True
             return False
 
-        def create_playList(select_dir):
+        def create_playList(selected_folder):
             m3u_list = []
-            for root, subdirs, files in os.walk(select_dir):
+
+            for root, subdirs, files in os.walk(selected_folder):
                 for filename in files:
-                    rel_dir = os.path.relpath(root, select_dir)
-                    if rel_dir == ".":
+                    rel_dir = os.path.relpath(root, selected_folder)
+                    if rel_dir == '.':
                         path = filename
                     else:
                         path = os.path.join(rel_dir, filename)
                     if is_music(path):
                         m3u_list.append(path)
+
             return m3u_list
 
-        m3u_path = os.path.join(select_dir, os.path.basename(select_dir) + ".m3u8")
+        m3u_path = os.path.join(
+            self.selected_folder,
+            os.path.basename(self.selected_folder) + '.m3u8')
 
         if os.path.exists(m3u_path):
-            os.remove(m3u_path)
+            is_overwrite = messagebox.askyesno(
+                'Already exist',
+                'The m3u8 file already exists in the output folder. Do you want to overwrite it?'
+            )
+            if not is_overwrite:
+                return
 
-        m3u_list = create_playList(select_dir)
+        m3u_list = create_playList(self.selected_folder)
 
-        f = open(m3u_path, 'w', encoding='utf-8')
-        for music in m3u_list:
-            f.write(unicodedata.normalize('NFC', music) + '\n')
-        f.close()
+        with open(m3u_path, 'w', encoding='utf-8') as f:
+            for music in m3u_list:
+                f.write('{}\n'.format(unicodedata.normalize('NFC', music)))
 
-        return m3u_path
+        messagebox.showinfo(r'Successfully generated', m3u_path)
 
-    def main(self):
-        body = gui.VBox(width='100%', height='100%')
-        main_container = gui.VBox(width=400, height=140,
-                        style={'align': 'center', 'border': '5px #FFAC55 solid'})
-        btn_container = gui.HBox(width=300, height=30)
-        link_to_github =  gui.Link('https://github.com/npes87184/PyM3UGenerator', 'Fork me here')
-
-        self.lbl = gui.Label('Please choose a folder')
-        self.select_bt = gui.Button('Select folder', width=100, height=30)
-        self.go_bt = gui.Button('Go', width=100, height=30)
-
-        self.select_bt.onclick.do(self.on_select_btn_pressed)
-        self.go_bt.onclick.do(self.on_go_btn_pressed)
-
-        btn_container.append(self.select_bt)
-        btn_container.append(self.go_bt)
-
-        main_container.append(self.lbl)
-        main_container.append(btn_container)
-        main_container.append(link_to_github)
-        body.append(main_container)
-
-        return body
 
 if __name__ == '__main__':
-    start(M3uGenerator)
+    root = tk.Tk()
+    root.minsize(640, 250)
+    root.title(r'M3uGenerator')
+    app = M3uGenerator(master=root)
+    app.mainloop()
