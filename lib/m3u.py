96ef7446d4bb9a072793fa807ecb5633b047d3e7
Yu-Chen Lin <npes87184@gmail.com>
2019-12-28 10:44:47 +0800

    Extract function for testing

--- lib/m3u.py
+++ lib/m3u.py
@@ -1,31 +1,31 @@
 import os
 import unicodedata
 
+def is_music(path):
+    support_extension = ['.mp3', '.m3u', '.wma', '.flac',
+        '.wav', '.mc', '.aac', '.m4a',
+        '.ape', '.dsf', '.dff']
+    _, extension = os.path.splitext(path)
+    if extension.lower() in support_extension:
+        return True
+    return False
+
+def create_playList(select_dir):
+    m3u_list = []
+    for root, _, files in os.walk(select_dir):
+        for filename in files:
+            rel_dir = os.path.relpath(root, select_dir)
+            if rel_dir == ".":
+                path = filename
+            else:
+                path = os.path.join(rel_dir, filename)
+            if is_music(path):
+                m3u_list.append(path)
+    return m3u_list
+
 def gen_m3u(select_dir):
     if not os.path.isdir(select_dir):
         return
-
-    def is_music(path):
-        support_extension = ['.mp3', '.m3u', '.wma', '.flac',
-            '.wav', '.mc', '.aac', '.m4a',
-            '.ape', '.dsf', '.dff']
-        _, extension = os.path.splitext(path)
-        if extension.lower() in support_extension:
-            return True
-        return False
-
-    def create_playList(select_dir):
-        m3u_list = []
-        for root, _, files in os.walk(select_dir):
-            for filename in files:
-                rel_dir = os.path.relpath(root, select_dir)
-                if rel_dir == ".":
-                    path = filename
-                else:
-                    path = os.path.join(rel_dir, filename)
-                if is_music(path):
-                    m3u_list.append(path)
-        return m3u_list
 
     m3u_path = os.path.join(select_dir, os.path.basename(select_dir) + ".m3u8")
 
